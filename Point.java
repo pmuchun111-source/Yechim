13dd7e49e7ebb976735e632bd6a411b5d2560f0a
pmuchun111-source <pmuchun111@gmail.com>
2026-05-13 09:07:42 +0500

    question 3

---
+++ Point.java
@@ -0,0 +1,17 @@
+public class Point {
+    private double x, y, z;
+    public Point(double  aa, double bb, double cc){
+        this.x=aa;
+        this.y=bb;
+        this.z=cc;
+    }
+    public double getA(){
+        return x;
+    }
+    public double getB(){
+        return y;
+    }
+    public double getC(){
+        return z;
+    }
+}
